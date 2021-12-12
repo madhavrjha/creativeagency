d1865cb07829c6d3d597e5b4cc74646da31e18ff
madhavrjha-lab <madhav@nextnow.africa>
2021-12-12 21:30:48 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+
+toggle.addEventListener('click', e => {
+  toggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
